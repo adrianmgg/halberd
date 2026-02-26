6e6a7463832e0388eeb21b6ad26b3610cbace87d
amgg <2adrian2@gmail.com>
2026-02-25 23:02:49 -0800

    more

--- src/parser.rs
+++ src/parser.rs
@@ -1,16 +1,36 @@
 use chumsky::input::ValueInput;
-use chumsky::prelude::*;
+use chumsky::{pratt::*, prelude::*};
 
-use crate::ast;
-use crate::lexer::Token;
+use crate::ast::{self, Expr};
+use crate::lexer::{self, Keyword, Token};
 
-fn expr_parser<'tokens, 'src: 'tokens, I>() -> impl Parser<
-    'tokens,
-    I,
-    Spanned<ast::Expr<'src>>,
-    chumsky::extra::Err<Rich<'src, Token<'src>, SimpleSpan>>,
-> + Clone
+type Err<'src> = chumsky::extra::Err<Rich<'src, Token<'src>, SimpleSpan>>;
+
+fn expr_parser<'tokens, 'src: 'tokens, I>()
+-> impl Parser<'tokens, I, Spanned<Expr<'src>>, Err<'src>> + Clone
 where
     I: ValueInput<'tokens, Token = Token<'src>, Span = SimpleSpan>,
 {
+    recursive(|expr| {
+        let ident = select_ref! { Token::Ident(x) => *x };
+        let atom = choice((
+            ident.map(Expr::Var),
+            just(Token::Keyword(Keyword::True)).to(Expr::LiteralBool(true)),
+            just(Token::Keyword(Keyword::False)).to(Expr::LiteralBool(true)),
+        ));
+        atom.spanned()
+            .pratt((infix(
+                left(1),
+                just(Token::Op {
+                    op: lexer::Op::Multiply,
+                    lifts: 0,
+                }),
+                |x, _, y, e| {
+                    Expr::InfixOp(Box::new(x), ast::InfixOp::Multiply, Box::new(y))
+                        .with_span(e.span())
+                },
+            ),))
+            .labelled("expression")
+            .as_context()
+    })
 }
