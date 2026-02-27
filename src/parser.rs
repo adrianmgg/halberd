f6b65a014038f7a5557c8ce1a2825c037bd9c2a6
amgg <2adrian2@gmail.com>
2026-02-26 16:11:11 -0800

    group (){}s in lexer

--- src/parser.rs
+++ src/parser.rs
@@ -11,15 +11,38 @@ pub fn parser<'tokens, 'src: 'tokens, I>() -> impl Parser<
     'tokens,
     I,
     // Spanned<Expr<'src>>,
-    Vec<Expr<'src>>,
+    Expr<'src>,
     extra::Err<Rich<'tokens, Token<'src>, SimpleSpan>>,
 >
 where
-    I: ValueInput<'tokens, Token = Token<'src>, Span = SimpleSpan>,
+    // I: ValueInput<'tokens, Token = Token<'src>, Span = SimpleSpan>,
+    I: BorrowInput<'tokens, Token = Token<'src>, Span = SimpleSpan>,
 {
-    let atom = choice((
-        just(Keyword::True).to(Expr::LiteralBool(true)),
-        just(Keyword::False).to(Expr::LiteralBool(false)),
-    ));
-    atom.repeated().collect()
+    recursive(|expr| {
+        // FIXME select_ref if end up using BorrowInput -- remove the other one when done
+        let ident = select_ref! { Token::Ident(x) => *x };
+        // let ident = select! { Token::Ident(x) => x };
+
+        let expr_boxed = expr.map(Box::new);
+
+        let atom = choice((
+            // true
+            just(Keyword::True).to(Expr::LiteralBool(true)),
+            // false
+            just(Keyword::False).to(Expr::LiteralBool(false)),
+            // foo
+            ident.map(Expr::Var),
+            // let name = ...
+            just(Keyword::Let)
+                .ignore_then(ident)
+                .then_ignore(just(Token::Equals))
+                .then(expr_boxed.clone())
+                .map(|(name, value)| Expr::Declaration { name, value }),
+        ));
+
+        let fn_args = just(Keyword::True).to(());
+        let fn_def = just(Keyword::Function).ignore_then(fn_args.nested_in());
+
+        atom.boxed()
+    })
 }
