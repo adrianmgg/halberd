a38205d3170e6e9ab11c54d69029c9bbfc5b67ae
amgg <2adrian2@gmail.com>
2026-03-15 18:29:14 -0700

    use enumset instead for capabilities

--- halberd/src/spv/mod.rs
+++ halberd/src/spv/mod.rs
@@ -3,5 +3,5 @@ pub mod asm;
 pub use crate::generated::spv::*;
 
 pub trait HasCapabilities {
-    fn capabilities(&self) -> impl Iterator<Item = operand_kind::Capability>;
+    fn capabilities(&self) -> enumset::EnumSet<operand_kind::Capability>;
 }
