c3af1b205dc34ea70c310dbc40f54bf92cd2538f
amgg <2adrian2@gmail.com>
2026-03-15 20:24:26 -0700

    missing derives & lint exclusions for codegen

--- halberd/src/spv/mod.rs
+++ halberd/src/spv/mod.rs
@@ -6,15 +6,21 @@ pub mod operand_kind {
     pub use crate::generated::spv::operand_kind::*;
 
     /// TODO
+    #[derive(Debug)]
     pub struct LiteralInteger;
     /// TODO
+    #[derive(Debug)]
     pub struct LiteralString;
     /// TODO
+    #[derive(Debug)]
     pub struct LiteralFloat;
     /// TODO
+    #[derive(Debug)]
     pub struct LiteralContextDependentNumber;
     /// TODO
+    #[derive(Debug)]
     pub struct LiteralExtInstInteger;
     /// TODO
+    #[derive(Debug)]
     pub struct LiteralSpecConstantOpInteger;
 }
