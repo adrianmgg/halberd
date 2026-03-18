ac14ea99cabbe76b234c94f4b6a350f94239d3a1
amgg <2adrian2@gmail.com>
2026-03-18 01:20:45 -0700

    instruction codegen mostly finished

--- halberd/src/iil/flat.rs
+++ halberd/src/iil/flat.rs
@@ -1,20 +1,9 @@
-use enum_dispatch::enum_dispatch;
-
 use crate::iil::{self, block};
 
-#[enum_dispatch]
 /// all F-IIL instructions
 pub trait Op {}
 
-/// an F-IIL instruction with a return value
-pub enum OpExpr {
-    OpIAdd(instruction::OpIAdd),
-}
-
-/// an F-IIL instruction with no return value
-pub enum OpVoid {
-    OpNop(instruction::OpNop),
-}
+pub use crate::generated::iil::flat::{OpExpr, OpVoid};
 
 pub enum Expr {
     Op(OpExpr),
@@ -25,15 +14,5 @@ pub type BlockValuedLocal = OpExpr;
 pub type BlockLocal = block::BlockLocal<BlockVoidLocal, BlockValuedLocal>;
 
 pub mod instruction {
-    use crate::iil::{self, block};
-
-    pub struct OpNop;
-    impl iil::f::Op for OpNop {}
-
-    pub struct OpIAdd {
-        pub r#type: iil::IntegerType,
-        pub lhs: block::BlockLocalRef,
-        pub rhs: block::BlockLocalRef,
-    }
-    impl iil::f::Op for OpIAdd {}
+    pub use crate::generated::iil::flat::instruction::*;
 }
