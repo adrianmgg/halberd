d06c2fa99fa891e7815efcf40b908569f97ad25e
amgg <2adrian2@gmail.com>
2026-04-03 17:48:38 -0700

    put sidecar map fns in a struct

--- halberd/src/compiler.rs
+++ halberd/src/compiler.rs
@@ -1,5 +1,5 @@
 use crate::{
-    ast::{self, NoSidecars, Sidecarred, Sidecars},
+    ast::{self, NoSidecars, SidecarFns, Sidecarred, Sidecars},
     types,
 };
 
@@ -16,16 +16,18 @@ fn foo<'a>(e: ast::Expr<'a, NoSidecars>) -> ast::Expr<'a, PartiallyTyped> {
     // we can trivially add a type already for anything whose type is definitive from just the
     // parsed ast, everything else we will need to do more work to figure out the type later
 
-    e.map_sidecars(|data, _| {
-        let r#type = match data {
-            ast::ExprData::LiteralInt(i) => Some(i.r#type.into()),
-            ast::ExprData::LiteralFloat(f) => Some(f.r#type.into()),
-            ast::ExprData::LiteralBool(_) => Some(types::Type::Bool),
-            ast::ExprData::InfixOp(_, _, _) => None,
-            ast::ExprData::Var(_) => None,
-            ast::ExprData::Declaration { .. } => None,
-            ast::ExprData::Block(_) => None,
-        };
-        PartiallyTypedExprData { r#type }
+    e.map_sidecars(&SidecarFns {
+        expr: |data: &ast::ExprData<'a>, _| {
+            let r#type = match data {
+                ast::ExprData::LiteralInt(i) => Some(i.r#type.into()),
+                ast::ExprData::LiteralFloat(f) => Some(f.r#type.into()),
+                ast::ExprData::LiteralBool(_) => Some(types::Type::Bool),
+                ast::ExprData::InfixOp(_, _, _) => None,
+                ast::ExprData::Var(_) => None,
+                ast::ExprData::Declaration { .. } => None,
+                ast::ExprData::Block(_) => None,
+            };
+            PartiallyTypedExprData { r#type }
+        },
     })
 }
