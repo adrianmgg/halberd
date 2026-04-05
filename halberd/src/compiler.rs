611a0319100ef91bda43198e7016197ea1f81e1c
amgg <2adrian2@gmail.com>
2026-04-05 00:31:16 -0700

    aaaaaaa

--- halberd/src/compiler.rs
+++ halberd/src/compiler.rs
@@ -17,18 +17,7 @@ pub fn foo<'a>(e: ast::Expr<'a, NoSidecars>) -> ast::Expr<'a, PartiallyTyped> {
     // parsed ast, everything else we will need to do more work to figure out the type later
 
     let mut e: ast::Expr<'a, PartiallyTyped> = e.map_sidecars(&SidecarFns {
-        expr: |data: &ast::ExprData<'a>, _| {
-            let r#type = match data {
-                ast::ExprData::LiteralInt(i) => Some(i.r#type.into()),
-                ast::ExprData::LiteralFloat(f) => Some(f.r#type.into()),
-                ast::ExprData::LiteralBool(_) => Some(types::Type::Bool),
-                ast::ExprData::InfixOp(_, _, _) => None,
-                ast::ExprData::Var(_) => None,
-                ast::ExprData::Declaration { .. } => None,
-                ast::ExprData::Block(_) => None,
-            };
-            PartiallyTypedExprData { r#type }
-        },
+        expr: |_: &ast::ExprData<'a>, _: ()| PartiallyTypedExprData { r#type: None },
     });
 
     e.iteratively_modify_sidecars(&SidecarFns {
@@ -36,36 +25,38 @@ pub fn foo<'a>(e: ast::Expr<'a, NoSidecars>) -> ast::Expr<'a, PartiallyTyped> {
             match sidecar.r#type {
                 Some(_) => false,
                 // FIXME rewrite this to handle the return bool automatically
-                None => match data {
-                    ast::ExprData::LiteralInt(_) => false,
-                    ast::ExprData::LiteralFloat(_) => false,
-                    ast::ExprData::LiteralBool(_) => false,
-                    ast::ExprData::InfixOp(lhs, op, rhs) => todo!(),
-                    ast::ExprData::Var(_) => false,
-                    // FIXME wait. is our ast wrong here WHOOPS
-                    ast::ExprData::Declaration { name: _, value } => match value.sidecar.r#type {
+                None => {
+                    let r#type = match data {
+                        ast::ExprData::LiteralInt(i) => Some(i.r#type.into()),
+                        ast::ExprData::LiteralFloat(f) => Some(f.r#type.into()),
+                        ast::ExprData::LiteralBool(_) => Some(types::Type::Bool),
+                        ast::ExprData::InfixOp(lhs, op, rhs) => match op.inner {
+                            ast::InfixOp::Add | ast::InfixOp::Subtract | ast::InfixOp::Multiply => {
+                                todo!()
+                            }
+                            ast::InfixOp::Divide => todo!(),
+                            ast::InfixOp::DotProduct => todo!(),
+                            ast::InfixOp::CrossProduct => todo!(),
+                            ast::InfixOp::MatrixMultiply => todo!(),
+                        },
+                        ast::ExprData::Var(_) => None,
+                        // FIXME wait. is our ast wrong here WHOOPS
+                        ast::ExprData::Declaration { name: _, value } => value.sidecar.r#type,
+                        ast::ExprData::Block(spanned) => match &spanned.inner.last {
+                            // blocks with no terminal expression get the type void
+                            None => Some(types::Type::Void),
+                            // blocks with a terminal expression get that expression's type if it has one
+                            Some(terminal) => terminal.sidecar.r#type,
+                        },
+                    };
+                    match r#type {
+                        None => false,
                         Some(r#type) => {
                             sidecar.r#type = Some(r#type);
                             true
                         }
-                        None => false,
-                    },
-                    ast::ExprData::Block(spanned) => match &spanned.inner.last {
-                        // blocks with no terminal expression get the type void
-                        None => {
-                            sidecar.r#type = Some(types::Type::Void);
-                            true
-                        }
-                        // blocks with a terminal expression get that expression's type if it has one
-                        Some(terminal) => match terminal.sidecar.r#type {
-                            Some(r#type) => {
-                                sidecar.r#type = Some(r#type);
-                                true
-                            }
-                            None => false,
-                        },
-                    },
-                },
+                    }
+                }
             }
         },
     });
