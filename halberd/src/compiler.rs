9b27727204dc7cdb21774e81f3216801614e1221
amgg <16143061+adrianmgg@users.noreply.github.com>
2026-04-16 15:25:25 -0700

    refactoring sidecar stuff

--- halberd/src/compiler.rs
+++ halberd/src/compiler.rs
@@ -4,182 +4,33 @@ use crate::{
     types::{self, prelude::*},
 };
 
-mod sidecars {
-    use std::{
-        fmt::{self, Debug},
-        marker::PhantomData,
-    };
-
-    use crate::{scope::ScopeId, types::Type};
-
-    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-    struct ExprSidecarInner {
-        scope: Option<ScopeId>,
-        r#type: Option<Type>,
-    }
-
-    #[derive(Clone, Copy, PartialEq, Eq)]
-    #[repr(transparent)]
-    pub struct ExprSidecar<S, T>(ExprSidecarInner, PhantomData<(S, T)>);
-
-    macro_rules! mk_exprsidecar_debug {
-        ($s:ty, $t:ty, $self:ident $(,($field:literal, $val:expr))*) => {
-            impl Debug for ExprSidecar<$s, $t> {
-                fn fmt(&$self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-                    f.debug_struct("ExprSidecar")
-                        $(.field($field, $val))*
-                        .finish()
-                }
-            }
-        };
-    }
-    mk_exprsidecar_debug!((), (), self);
-    mk_exprsidecar_debug!(Option<ScopeId>, (), self, ("scope", &self.scope_maybe()));
-    mk_exprsidecar_debug!(ScopeId, (), self, ("scope", &self.scope()));
-    mk_exprsidecar_debug!(
-        ScopeId,
-        Option<Type>,
-        self,
-        ("scope", &self.scope()),
-        ("type", &self.type_maybe())
-    );
-    mk_exprsidecar_debug!(
-        ScopeId,
-        Type,
-        self,
-        ("scope", &self.scope()),
-        ("type", &self.r#type())
-    );
-
-    impl Default for ExprSidecar<(), ()> {
-        fn default() -> Self { Self(ExprSidecarInner { scope: None, r#type: None }, PhantomData) }
-    }
-
-    impl<S, T> ExprSidecar<S, T> {
-        pub fn with_scope_none(&self) -> ExprSidecar<Option<ScopeId>, T> {
-            ExprSidecar(
-                ExprSidecarInner { scope: None, r#type: self.0.r#type },
-                PhantomData,
-            )
-        }
-
-        // FIXME wait this should be taking self as owned shouldn't it uh oh
-        pub fn with_scope(&self, id: ScopeId) -> ExprSidecar<ScopeId, T> {
-            ExprSidecar(
-                ExprSidecarInner { scope: Some(id), r#type: self.0.r#type },
-                PhantomData,
-            )
-        }
-
-        pub fn with_type_none(&self) -> ExprSidecar<S, Option<Type>> {
-            ExprSidecar(
-                ExprSidecarInner { scope: self.0.scope, r#type: None },
-                PhantomData,
-            )
-        }
-
-        pub fn with_type(&self, r#type: Type) -> ExprSidecar<S, Type> {
-            ExprSidecar(
-                ExprSidecarInner { scope: self.0.scope, r#type: Some(r#type) },
-                PhantomData,
-            )
-        }
-    }
-
-    impl<T> ExprSidecar<Option<ScopeId>, T> {
-        pub fn scope_maybe(&self) -> Option<ScopeId> { self.0.scope }
-
-        pub fn scope_maybe_mut(&mut self) -> &mut Option<ScopeId> { &mut self.0.scope }
-
-        pub fn try_with_scope_definitely(self) -> Option<ExprSidecar<ScopeId, T>> {
-            if self.0.scope.is_none() {
-                None
-            } else {
-                Some(ExprSidecar(self.0, PhantomData))
-            }
-        }
-    }
-    impl<T> ExprSidecar<ScopeId, T> {
-        pub fn scope(&self) -> ScopeId { unsafe { self.0.scope.unwrap_unchecked() } }
-
-        pub fn scope_mut(&mut self) -> &mut ScopeId {
-            unsafe { self.0.scope.as_mut().unwrap_unchecked() }
-        }
-    }
-    impl<S> ExprSidecar<S, Option<Type>> {
-        pub fn type_maybe(&self) -> Option<Type> { self.0.r#type }
-
-        pub fn type_maybe_mut(&mut self) -> &mut Option<Type> { &mut self.0.r#type }
-
-        pub fn try_with_type_definitely(self) -> Option<ExprSidecar<S, Type>> {
-            if self.0.r#type.is_none() {
-                None
-            } else {
-                Some(ExprSidecar(self.0, PhantomData))
-            }
-        }
-    }
-    impl<S> ExprSidecar<S, Type> {
-        pub fn r#type(&self) -> Type { unsafe { self.0.r#type.unwrap_unchecked() } }
-
-        pub fn type_mut(&mut self) -> &mut Type {
-            unsafe { self.0.r#type.as_mut().unwrap_unchecked() }
-        }
-    }
-
-    impl<T> From<ExprSidecar<(), T>> for ExprSidecar<Option<ScopeId>, T> {
-        fn from(value: ExprSidecar<(), T>) -> Self { Self(value.0, PhantomData) }
-    }
-    impl<T> TryFrom<ExprSidecar<Option<ScopeId>, T>> for ExprSidecar<ScopeId, T> {
-        type Error = ();
-
-        fn try_from(value: ExprSidecar<Option<ScopeId>, T>) -> Result<Self, Self::Error> {
-            if value.scope_maybe().is_none() {
-                Err(())
-            } else {
-                let inner = value.0;
-                Ok(Self(inner, PhantomData))
-            }
-        }
-    }
-
-    impl<S> From<ExprSidecar<S, ()>> for ExprSidecar<S, Option<Type>> {
-        fn from(value: ExprSidecar<S, ()>) -> Self { Self(value.0, PhantomData) }
-    }
-    impl<S> TryFrom<ExprSidecar<S, Option<Type>>> for ExprSidecar<S, Type> {
-        type Error = ();
-
-        fn try_from(value: ExprSidecar<S, Option<Type>>) -> Result<Self, Self::Error> {
-            if value.type_maybe().is_none() {
-                Err(())
-            } else {
-                let inner = value.0;
-                Ok(Self(inner, PhantomData))
-            }
-        }
-    }
-
-    // FIXME give this a better name
-    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-    pub struct TheSidecars<Expr>(PhantomData<Expr>);
-    impl<Expr: PartialEq + Clone + std::fmt::Debug> crate::ast::Sidecars for TheSidecars<Expr> {
-        type Expr = Expr;
-    }
-}
+// FIXME need to disambiguate the naming between
+//       sidecars (the AST's implementation of the sidecars system)
+//       sidecars (the compiler's sidecar types for use with that system)
+mod sidecars;
 
 pub(crate) use sidecars::ExprSidecar;
+use sidecars::{ExprSidecarS, ExprSidecarT};
 
-/// nothing
-pub(crate) type NoSidecars = sidecars::TheSidecars<ExprSidecar<(), ()>>;
-/// some scopes
-pub(crate) type PhasePartiallyScoped = sidecars::TheSidecars<ExprSidecar<Option<ScopeId>, ()>>;
-/// just scope
-pub(crate) type PhaseFullyScoped = sidecars::TheSidecars<ExprSidecar<ScopeId, ()>>;
-/// scope, and some types
-pub(crate) type PhasePartiallyTyped =
-    sidecars::TheSidecars<ExprSidecar<ScopeId, Option<types::Type>>>;
-/// scope and fully typed
-pub(crate) type PhaseFullyTyped = sidecars::TheSidecars<ExprSidecar<ScopeId, types::Type>>;
+macro_rules! named_sidecars {
+    ($($(#[$m:meta])* $name:ident = $expr_scope:ty, $expr_type:ty;)*) => {
+        $($(#[$m])*
+        pub(crate) type $name = sidecars::TheSidecars<ExprSidecar<$expr_scope, $expr_type>>;
+        )*
+    };
+}
+named_sidecars! {
+    /// nothing
+    NoSidecars = (), ();
+    /// some scopes
+    PhasePartiallyScoped = Option<ScopeId>, ();
+    /// just scope
+    PhaseFullyScoped = ScopeId, ();
+    /// scope, and some types
+    PhasePartiallyTyped = ScopeId, Option<types::Type>;
+    /// scope and fully typed
+    PhaseFullyTyped = ScopeId, types::Type;
+}
 
 pub fn foo<'a>(
     e: ast::Expr<'a, NoSidecars>,
@@ -193,11 +44,12 @@ pub fn foo<'a>(
         e.map_sidecars(&mut SidecarFns { expr: &mut |_, car| car.with_scope_none() });
 
     // populate scopes
+    // FIXME this doesn't need to be using adding its own second level of Option...
     e.iteratively_modify_sidecars_2(&mut SidecarFns {
         expr: &mut |data,
                     car: &mut ExprSidecar<Option<ScopeId>, ()>,
                     ctx: SidecarWalkContexts<Option<ScopeId>>| {
-            match car.scope_maybe_mut() {
+            match car.scope_mut() {
                 Some(scope) => (false, Some(*scope)),
                 scope @ None => {
                     let super_scope = ctx.prior_sibling.flatten().or(ctx.parent);
@@ -217,7 +69,7 @@ pub fn foo<'a>(
     // ensure all scopes populated
     e.validate_sidecars(&mut SidecarFns {
         expr: &mut |data, car| {
-            car.scope_maybe().is_none().then_some(
+            car.scope().is_none().then_some(
                 ariadne::Report::build(ariadne::ReportKind::Error, data.span().into_range())
                     .with_message("spans were not correctly applied everywhere! (this is an internal compiler error, not a problem with your code)")
                     .finish(),
@@ -236,7 +88,7 @@ pub fn foo<'a>(
     // now start actually populating the types
     e.iteratively_modify_sidecars(&mut SidecarFns {
         expr: |data: &ast::ExprData<'a, PhasePartiallyTyped>, sidecar: &mut ExprSidecar<_, _>| {
-            match sidecar.type_maybe_mut() {
+            match sidecar.type_mut() {
                 Some(_) => false,
                 sidecar_type @ None => {
                     let r#type = infer_expr_type(data);
@@ -254,7 +106,7 @@ pub fn foo<'a>(
 
     e.validate_sidecars(&mut SidecarFns {
         expr: &mut |data, car| {
-            car.type_maybe().is_none().then_some(
+            car.r#type().is_none().then_some(
                 ariadne::Report::build(ariadne::ReportKind::Error, data.span().into_range())
                     .with_message("unable to infer type")
                     .finish(),
@@ -274,8 +126,8 @@ fn infer_expr_type<'a>(data: &ast::ExprData<'a, PhasePartiallyTyped>) -> Option<
         ast::ExprData::LiteralFloat(f) => Some(f.r#type.into()),
         ast::ExprData::LiteralBool(_) => Some(types::Type::Bool),
         ast::ExprData::InfixOp(lhs, op, rhs) => {
-            let lhs_type = lhs.sidecar.type_maybe();
-            let rhs_type = rhs.sidecar.type_maybe();
+            let lhs_type = lhs.sidecar.r#type();
+            let rhs_type = rhs.sidecar.r#type();
             let lhs_and_rhs = try { (lhs_type?, rhs_type?) };
             match op.inner {
                 ast::InfixOp::Add | ast::InfixOp::Subtract | ast::InfixOp::Multiply =>
@@ -318,12 +170,12 @@ fn infer_expr_type<'a>(data: &ast::ExprData<'a, PhasePartiallyTyped>) -> Option<
         }
         ast::ExprData::Var(_) => None,
         // FIXME wait. is our ast wrong here WHOOPS
-        ast::ExprData::Declaration { name: _, value } => value.sidecar.type_maybe(),
+        ast::ExprData::Declaration { name: _, value } => value.sidecar.r#type(),
         ast::ExprData::Block(spanned) => match &spanned.inner.last {
             // blocks with no terminal expression get the type void
             None => Some(types::Type::Void),
             // blocks with a terminal expression get that expression's type if it has one
-            Some(terminal) => terminal.sidecar.type_maybe(),
+            Some(terminal) => terminal.sidecar.r#type(),
         },
     }
 }
---
+++ halberd/src/compiler/sidecars.rs
@@ -0,0 +1,174 @@
+use std::{
+    fmt::{self, Debug},
+    marker::PhantomData,
+};
+
+use crate::{scope::ScopeId, types::Type};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct ExprSidecarInner {
+    scope: Option<ScopeId>,
+    r#type: Option<Type>,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+pub struct ExprSidecar<S, T>(ExprSidecarInner, PhantomData<(S, T)>);
+
+// FIXME names
+pub(crate) trait ExprSidecarS<S> {
+    fn scope(&self) -> S;
+    fn scope_mut(&mut self) -> &mut S;
+}
+pub(crate) trait ExprSidecarT<T> {
+    fn r#type(&self) -> T;
+    fn type_mut(&mut self) -> &mut T;
+}
+
+impl<S: Debug, T: Debug> Debug for ExprSidecar<S, T>
+where Self: ExprSidecarS<S> + ExprSidecarT<T>
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // FIXME oh shit wait is this making a local copy just to give the ref of it... should we
+        //       add a `.scope_ref` or make `.scope` return a ref or smth? hmm
+        f.debug_struct("ExprSidecar")
+            .field("scope", &self.scope())
+            .field("type", &self.r#type())
+            .finish()
+    }
+}
+
+macro_rules! esx {
+    (@ess; $self:ident; $( $ty:ty { scope -> $body1:tt scope_mut -> $body2:tt } )* ) => {
+        $( impl<T> ExprSidecarS<$ty> for ExprSidecar<$ty, T> {
+            fn scope(&$self) -> $ty $body1
+            fn scope_mut(&mut $self) -> &mut $ty $body2
+        } )*
+    };
+    (@est; $self:ident; $( $ty:ty { r#type -> $body1:tt type_mut -> $body2:tt } )* ) => {
+        $( impl<S> ExprSidecarT<$ty> for ExprSidecar<S, $ty> {
+            fn r#type(&$self) -> $ty $body1
+            fn type_mut(&mut $self) -> &mut $ty $body2
+        } )*
+    };
+}
+
+esx! {@ess; self;
+    () {
+        scope -> {}
+        // pretty sure this is ok - https://old.reddit.com/r/rust/comments/rjhiod/is_it_safe_to_create_references_to_zerosized/hp3oqr1/
+        scope_mut -> { unsafe { &mut *(std::mem::align_of::<()>() as *mut ()) } }
+    }
+    Option<ScopeId> {
+        scope -> { self.0.scope }
+        scope_mut -> { &mut self.0.scope }
+    }
+    ScopeId {
+        scope -> { unsafe { self.0.scope.unwrap_unchecked() } }
+        scope_mut -> { unsafe { self.0.scope.as_mut().unwrap_unchecked() } }
+    }
+}
+esx! {@est; self;
+    () {
+        r#type -> {}
+        // pretty sure this is ok - https://old.reddit.com/r/rust/comments/rjhiod/is_it_safe_to_create_references_to_zerosized/hp3oqr1/
+        type_mut -> { unsafe { &mut *(std::mem::align_of::<()>() as *mut ()) } }
+    }
+    Option<Type> {
+        r#type -> { self.0.r#type }
+        type_mut -> { &mut self.0.r#type }
+    }
+    Type {
+        r#type -> { unsafe { self.0.r#type.unwrap_unchecked() } }
+        type_mut -> { unsafe { self.0.r#type.as_mut().unwrap_unchecked() } }
+    }
+}
+
+impl Default for ExprSidecar<(), ()> {
+    fn default() -> Self { Self(ExprSidecarInner { scope: None, r#type: None }, PhantomData) }
+}
+
+macro_rules! es_withs {
+    (
+        $self:ident, $s:ident, $t:ident;
+        $($name:ident ($($arg:ident: $arg_ty:ty),*) -> <$rs:ty,$rt:ty> = $es:expr , $et:expr ; )*
+    ) => {
+        impl<$s, $t> ExprSidecar<$s, $t> {
+            // FIXME wait this should be taking self as owned shouldn't it uh oh
+            $(pub fn $name (&$self $(,$arg:$arg_ty)*) -> ExprSidecar<$rs, $rt> {
+                ExprSidecar(
+                    ExprSidecarInner { scope: $es, r#type: $et },
+                    PhantomData,
+                )
+            })*
+        }
+    };
+}
+
+es_withs! {self, S, T;
+    with_scope_none() -> <Option<ScopeId>, T> = None, self.0.r#type;
+    with_scope(s: ScopeId) -> <ScopeId, T> = Some(s), self.0.r#type;
+    with_type_none() -> <S, Option<Type>> = self.0.scope, None;
+    with_type(t: Type) -> <S, Type> = self.0.scope, Some(t);
+}
+
+impl<T> ExprSidecar<Option<ScopeId>, T> {
+    pub fn try_with_scope_definitely(self) -> Option<ExprSidecar<ScopeId, T>> {
+        if self.0.scope.is_none() {
+            None
+        } else {
+            Some(ExprSidecar(self.0, PhantomData))
+        }
+    }
+}
+
+impl<S> ExprSidecar<S, Option<Type>> {
+    pub fn try_with_type_definitely(self) -> Option<ExprSidecar<S, Type>> {
+        if self.0.r#type.is_none() {
+            None
+        } else {
+            Some(ExprSidecar(self.0, PhantomData))
+        }
+    }
+}
+
+impl<T> From<ExprSidecar<(), T>> for ExprSidecar<Option<ScopeId>, T> {
+    fn from(value: ExprSidecar<(), T>) -> Self { Self(value.0, PhantomData) }
+}
+
+impl<T> TryFrom<ExprSidecar<Option<ScopeId>, T>> for ExprSidecar<ScopeId, T> {
+    type Error = ();
+
+    fn try_from(value: ExprSidecar<Option<ScopeId>, T>) -> Result<Self, Self::Error> {
+        if value.scope().is_none() {
+            Err(())
+        } else {
+            let inner = value.0;
+            Ok(Self(inner, PhantomData))
+        }
+    }
+}
+
+impl<S> From<ExprSidecar<S, ()>> for ExprSidecar<S, Option<Type>> {
+    fn from(value: ExprSidecar<S, ()>) -> Self { Self(value.0, PhantomData) }
+}
+
+impl<S> TryFrom<ExprSidecar<S, Option<Type>>> for ExprSidecar<S, Type> {
+    type Error = ();
+
+    fn try_from(value: ExprSidecar<S, Option<Type>>) -> Result<Self, Self::Error> {
+        if value.r#type().is_none() {
+            Err(())
+        } else {
+            let inner = value.0;
+            Ok(Self(inner, PhantomData))
+        }
+    }
+}
+
+// FIXME give this a better name
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct TheSidecars<Expr>(PhantomData<Expr>);
+impl<Expr: PartialEq + Clone + std::fmt::Debug> crate::ast::Sidecars for TheSidecars<Expr> {
+    type Expr = Expr;
+}
